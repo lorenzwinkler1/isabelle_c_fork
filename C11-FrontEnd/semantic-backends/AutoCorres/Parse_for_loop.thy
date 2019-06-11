1d6a53745ae86ef08aa3099ad4561f8a480990b4
bu <wolff@lri.fr>
2019-06-11 14:48:24 +0200

    Improved Example Set for AutoCorres

---
+++ C11-FrontEnd/semantic-backends/AutoCorres/Parse_for_loop.thy
@@ -0,0 +1,84 @@
+(*
+ * Copyright 2018-2019 Université Paris-Saclay, Univ. Paris-Sud, France
+ * Copyright 2014, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ *)
+
+chapter\<open>Example \<close>
+
+text\<open>This example is used to demonstrate Isabelle/C/Autocorres in a version that uses 
+AutoCorres Annotations addressed to AutoCorres. The example annotation is found in (C)-function g.
+Note that this proceeding is not necessarily recommended - since AutoCorres does not support
+semantic PIDE support, editing feedback is limited and the workflow somewhat clumpsy.\<close>
+
+theory Parse_for_loop
+imports
+  "AutoCorres.AutoCorres"
+  "HOL-Computational_Algebra.Primes"
+begin
+
+C\<open>
+/*
+ * Copyright 2014, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
+
+//  Activating the AutoCorres backend of Isabe;lle/C.
+//@ install_autocorres parse_for_loop [ ts_rules = nondet, unsigned_word_abs = f g h f2 ]
+
+
+/* also tests
+   - post-increment and decrement (which are common in for loops)
+   - arrays on the heap and stack (treated differently in VCG-land)
+*/
+
+int *f(int *i, int c)
+{
+  unsigned j;
+  for (j = 0; j < 4; j++) i[j] = i[j] + c;
+  i[0]++;
+  return i;
+}
+
+int g(int c)
+{
+  for (unsigned int j = 10; 0 < j; j--)
+    /** INVARIANT: "\<lbrace> 0 <= \<acute>j & \<acute>j <= 10 \<rbrace>" */
+    {
+      c = c + j;
+    }
+  return c;
+}
+
+int h(int c)
+{
+  int a[10];
+  for (unsigned int j = 0; j < 10; j++) a[j] = j;
+  a[0] = a[1] + a[2];
+  return a[0];
+}
+
+int f2(int *a)
+{
+  int j, res;
+  for (j=0,res=0; j < 32; j += 4) { res += a[j]; }
+  return res;
+}
+
+\<close>
+
+find_theorems name:"parse_for_loop"
+
+thm parse_for_loop.g'_def[simp]
+
+end
