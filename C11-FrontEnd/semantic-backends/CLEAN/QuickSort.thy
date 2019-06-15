262540b84b89cf1384da8bbd5da6d97145790c6b
bu <wolff@lri.fr>
2019-06-15 22:31:23 +0200

    Added encoding for 'partition'

--- C11-FrontEnd/semantic-backends/CLEAN/QuickSort.thy
+++ C11-FrontEnd/semantic-backends/CLEAN/QuickSort.thy
@@ -35,18 +35,18 @@ funct swap (i::nat, j :: nat) returns unit
        \<open>A[j] := tmp\<close>
       \<close>
 
-funct partition (li::int, hi::int) returns int
+funct partition (lo::nat, hi::nat) returns nat
      pre  "True"
      post "True"
-     local_vars pivot :: int  i :: int  j :: int
+     local_vars pivot :: nat  i :: nat  j :: nat
      \<open>\<open>pivot := A!hi\<close> ;-
       \<open>i := lo\<close> ;-
       for\<^sub>C\<^sub>L\<^sub>E\<^sub>A\<^sub>N (j=lo,  hi - 1 ,j++)  
          \<open>if\<^sub>C\<^sub>L\<^sub>E\<^sub>A\<^sub>N \<open>A!j < pivot\<close> then swap(i,j);- \<open>i := i + 1\<close>
                                else Skip;-
-          swap(i,j);-
-          return(i)
          \<close>
+      swap(i,j);-
+      return(i)
      \<close>
          
 funct quicksort(lo::int, hi::int) returns unit
@@ -59,23 +59,15 @@ funct quicksort(lo::int, hi::int) returns unit
         quicksort(A, p + 1, hi)
       else Skip\<close>
       
-
 *)
 
 
 global_vars state
     A :: "int list "
+ML\<open> val Type(s,t) = StateMgt_core.get_state_type_global @{theory}; \<close>
 
 
-ML\<open>
-val Type(s,t) = StateMgt_core.get_state_type_global @{theory};
-\<close>
-
-         ML\<open>Record.last_extT @{typ state}\<close>
-         ML\<open>Record.get_extension @{theory} "state"\<close>
-
-
-find_theorems name:state
+subsection \<open>Encoding swap in CLEAN\<close>
 
 (* for some strange reason, "result" is no longer a term. term "result" crashes. *)
 local_vars local_swap_state  
@@ -84,63 +76,117 @@ local_vars local_swap_state
 
 
 definition push_local_state_swap :: "(unit,'a local_swap_state_scheme) MON\<^sub>S\<^sub>E"
-  where   "push_local_state_swap \<sigma> = Some((),
-                                          \<sigma>\<lparr>local_swap_state.tmp := 
-                                               undefined # local_swap_state.tmp \<sigma> \<rparr>)"
+  where   "push_local_state_swap \<sigma> = 
+                    Some((),\<sigma>\<lparr>local_swap_state.tmp :=  undefined # local_swap_state.tmp \<sigma> \<rparr>)"
 
 definition pop_local_state_swap :: "(unit,'a local_swap_state_scheme) MON\<^sub>S\<^sub>E"
-  where   "pop_local_state_swap \<sigma> = Some(hd(local_swap_state.res \<sigma>), \<comment> \<open> recall : returns unit \<close>
-                                        \<sigma>\<lparr>local_swap_state.tmp:= tl( local_swap_state.tmp \<sigma>) \<rparr>)"
-term "Clean.syntax_assign"
-term "B[x:=(B!j)]"
+  where   "pop_local_state_swap \<sigma> = 
+                    Some(hd(local_swap_state.res \<sigma>), \<comment> \<open> recall : returns op value \<close>
+                                                     \<comment> \<open> which happens to be unit \<close>
+                         \<sigma>\<lparr>local_swap_state.tmp:= tl( local_swap_state.tmp \<sigma>) \<rparr>)"
 
 
-
-term "assign_local tmp_update (\<lambda>\<sigma>. A \<sigma> ! i )"  
-term "assign(\<lambda>\<sigma>. ((upd o map_hd) (f \<sigma>)) \<sigma>)"
-
-term "assign "
-term "(A \<sigma>[i := A \<sigma> ! i]) = list_update (A \<sigma>) (i) (A \<sigma> ! i)"
-
-term "assign(\<lambda>\<sigma>. ((A_update ) (\<lambda>_. list_update (A \<sigma>) (i) (A \<sigma> ! i))) \<sigma>)"
-term " ((A_update o map_hd) f)"
-term " block"
-
-
-term "assign_global A_update (\<lambda>\<sigma>. list_update (A \<sigma>) (i) (A \<sigma> ! j))"
-definition swap_body :: "nat => nat =>  (unit,'a local_swap_state_scheme) MON\<^sub>S\<^sub>E"
-    where "swap_body i j \<equiv> ((assign_local tmp_update (\<lambda>\<sigma>. A \<sigma> ! i ))   ;-
+definition swap_core :: "nat \<Rightarrow> nat \<Rightarrow>  (unit,'a local_swap_state_scheme) MON\<^sub>S\<^sub>E"
+    where "swap_core i j \<equiv> ((assign_local tmp_update (\<lambda>\<sigma>. A \<sigma> ! i ))   ;-
                             (assign_global A_update (\<lambda>\<sigma>. list_update (A \<sigma>) (i) (A \<sigma> ! j))) ;- 
                             (assign_global A_update (\<lambda>\<sigma>. list_update (A \<sigma>) (j) ((hd o tmp) \<sigma>))))" 
-thm swap_body_def
+thm swap_core_def
+(* future input cartouche syntax should be: 
+definition swap_core :: "nat => nat =>  (unit,'a local_swap_state_scheme) MON\<^sub>S\<^sub>E"
+   where  "swap_core i j \<equiv> (\<open>tmp := A!i\<close>       ;-
+                            \<open>A[i] := A!j)\<close> ;-   
+                            \<open>A[j] := tmp\<close>)"
+*)
 
-definition swap :: "nat => nat =>  (unit,'a local_swap_state_scheme) MON\<^sub>S\<^sub>E"
-   where  "swap i j \<equiv> (\<open>tmp := A!i\<close>       ;-
-                       \<open>A := (A[i:=(A!j)])\<close> ;-   
-                       \<open>A := (A[j:=tmp])\<close>)"
+(* a block manages the "dynamically" created fresh instances for the local variables of swap *)
+definition swap :: "nat \<Rightarrow> nat \<Rightarrow>  (unit,'a local_swap_state_scheme) MON\<^sub>S\<^sub>E"
+  where   "swap i j \<equiv> block\<^sub>C push_local_state_swap (swap_core i j) pop_local_state_swap"
+        
+(* TODO : model the contract language. *)
+definition swap_contract :: "nat \<Rightarrow> nat \<Rightarrow>  (unit,'a local_swap_state_scheme) MON\<^sub>S\<^sub>E"
+  where   "swap_contract i j \<equiv> undefined "
 
-
+subsection \<open>Encoding partition in CLEAN\<close>
 
 local_vars  local_partition_state
     pivot  :: "int list"
-    i      :: "int list" 
-    res   :: "int list"
-
+    i      :: "nat list"
+    j      :: "nat list"
+    res    :: "nat list"
 
 (* this implies the definitions : *)
-definition push_local_partition_state :: "(unit,local_partition_state) MON\<^sub>S\<^sub>E"
-  where "push_local_partition_state \<sigma> = Some((),
-                                   \<sigma>\<lparr>local_partition_state.pivot:= undefined # local_partition_state.pivot \<sigma>, 
-                                     local_partition_state.i:=undefined # local_partition_state.i \<sigma>, 
-                                     local_partition_state.res := undefined # local_partition_state.res \<sigma> \<rparr>)"
+definition push_local_partition_state :: "(unit, 'a local_partition_state_scheme) MON\<^sub>S\<^sub>E"
+  where   "push_local_partition_state \<sigma> = Some((),
+                        \<sigma>\<lparr>local_partition_state.pivot := undefined # local_partition_state.pivot \<sigma>, 
+                          local_partition_state.i     := undefined # local_partition_state.i \<sigma>, 
+                          local_partition_state.j     := undefined # local_partition_state.j \<sigma>, 
+                          local_partition_state.res   := undefined # local_partition_state.res \<sigma> \<rparr>)"
 
-definition pop_local_partition_state :: "(int,local_partition_state) MON\<^sub>S\<^sub>E" 
-  where "pop_local_partition_state \<sigma> = Some(hd(local_partition_state.res \<sigma>),
-                                  \<sigma>\<lparr>local_partition_state.pivot:= tl( local_partition_state.pivot \<sigma>), 
-                                    local_partition_state.i:=tl( local_partition_state.i \<sigma>), 
-                                    local_partition_state.res :=tl( local_partition_state.res \<sigma>) \<rparr>)"
+definition pop_local_partition_state :: "(nat,'a local_partition_state_scheme) MON\<^sub>S\<^sub>E" 
+  where   "pop_local_partition_state \<sigma> = Some(hd(local_partition_state.res \<sigma>),
+                       \<sigma>\<lparr>local_partition_state.pivot := tl(local_partition_state.pivot \<sigma>), 
+                         local_partition_state.i     := tl(local_partition_state.i \<sigma>), 
+                         local_partition_state.j     := tl(local_partition_state.j \<sigma>), 
+                         local_partition_state.res   := tl(local_partition_state.res \<sigma>) \<rparr>)"
 
-term "B[k:=(B!j)]"
+
+definition partition_core :: "nat \<Rightarrow> nat \<Rightarrow>  (unit,'a local_partition_state_scheme) MON\<^sub>S\<^sub>E"
+  where   "partition_core lo hi \<equiv> 
+              ((assign_local pivot_update (\<lambda>\<sigma>. A \<sigma> ! hi ))   ;- 
+               (assign_local i_update (\<lambda>\<sigma>. lo )) ;-
+ 
+               (assign_local j_update (\<lambda>\<sigma>. lo )) ;-
+               (while\<^sub>C (\<lambda>\<sigma>. (hd o j) \<sigma> \<le> hi - 1 ) 
+                do (if\<^sub>C (\<lambda>\<sigma>. A \<sigma> ! (hd o j) \<sigma> < (hd o pivot)\<sigma> ) 
+                    then (\<lambda>\<sigma>. (swap ((hd o i) \<sigma>)  ((hd o j) \<sigma>))  \<sigma>) ;-
+                         (assign_local i_update (\<lambda>\<sigma>. ((hd o i) \<sigma>) + 1))
+                    else skip\<^sub>S\<^sub>E 
+                    fi) 
+                od) ;-
+               (assign_local j_update (\<lambda>\<sigma>. ((hd o j) \<sigma>) + 1)) ;-
+
+               (\<lambda>\<sigma>. (swap ((hd o i) \<sigma>)  ((hd o j) \<sigma>))  \<sigma>) ;-
+               (assign_local res_update (\<lambda>\<sigma>. (hd o i) \<sigma>))  \<comment> \<open> the meaning of the return stmt \<close>
+               )"
+
+thm partition_core_def
+
+(* a block manages the "dynamically" created fresh instances for the local variables of swap *)
+definition partition :: "nat \<Rightarrow> nat \<Rightarrow>  (nat,'a local_partition_state_scheme) MON\<^sub>S\<^sub>E"
+  where   "partition lo hi \<equiv> block\<^sub>C push_local_partition_state 
+                                    (partition_core lo hi) 
+                                    pop_local_partition_state"
+        
+(* TODO : model the contract language. *)
+definition partition_contract :: "nat \<Rightarrow> nat \<Rightarrow>  (nat,'a local_swap_state_scheme) MON\<^sub>S\<^sub>E"
+  where   "partition_contract lo hi \<equiv> undefined "
+
+(*
+    \<open>\<open>pivot := A!hi\<close> ;-
+      \<open>i := lo\<close> ;-
+      for\<^sub>C\<^sub>L\<^sub>E\<^sub>A\<^sub>N (j=lo,  hi - 1 ,j++)  
+         \<open>if\<^sub>C\<^sub>L\<^sub>E\<^sub>A\<^sub>N \<open>A!j < pivot\<close> then swap(i,j);- \<open>i := i + 1\<close>
+                               else Skip;-
+         \<close>
+      swap(i,j);-
+      return(i)
+     \<close>
+*)         
+
+term "Clean.syntax_assign"
+term "B[x:=(B!n)]"
+term "assign_local tmp_update (\<lambda>\<sigma>. A \<sigma> ! n )"  
+term "assign(\<lambda>\<sigma>. ((upd o map_hd) (f \<sigma>)) \<sigma>)"
+
+term "assign "
+term "(A \<sigma>[n := A \<sigma> ! m]) = list_update (A \<sigma>) (n) (A \<sigma> ! m)"
+
+term "assign(\<lambda>\<sigma>. ((A_update ) (\<lambda>_. list_update (A \<sigma>) (n) (A \<sigma> ! n))) \<sigma>)"
+term " ((A_update o map_hd) f)"
+term " body\<^sub>C push_local_state_swap pop_local_state_swap "
+term "assign_global A_update (\<lambda>\<sigma>. list_update (A \<sigma>) (n) (A \<sigma> ! m))"
+
+term "B[k:=(B!m)]"
 
 
 end
