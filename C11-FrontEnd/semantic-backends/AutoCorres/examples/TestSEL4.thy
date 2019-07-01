c80073dd3feda36009fc6b17e56af78185b964f7
Frédéric Tuong <tuong@users.noreply.github.com>
2019-07-01 18:51:31 +0200

    clean

--- C11-FrontEnd/semantic-backends/AutoCorres/examples/TestSEL4.thy
+++ C11-FrontEnd/semantic-backends/AutoCorres/examples/TestSEL4.thy
@@ -24,7 +24,6 @@ declare [[ML_print_depth = 500]] \<comment> \<open>Any number large enough for @
  * Test to check if all C parsed values are equal to the original parser modulo positions.
  *)
 
-
 install_C_file all_parsing no_cpp parse_then_stop
                \<comment> \<open>The following file can be meanwhile CTRL-clicked on it:\<close>
                \<open>../../../../l4v/generated/spec/cspec/c/build/ARM/kernel_all.c_pp\<close>
@@ -249,8 +248,6 @@ seL4_MessageInfo_get_extraCaps(seL4_MessageInfo_t seL4_MessageInfo) {
     return ret;
 }
 \<close>
-
-
 
 context \<comment> \<open>The following binding can be meanwhile CTRL-clicked on it:\<close>
         kernel_not_all
